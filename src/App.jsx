3367ff5afcebc174d6be062fa285cddc07f32f2d
LEE-NS <ogawa9341@gmail.com>
2024-05-20 22:06:18 +0900

    Update complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from "react"
 
 function App() {
   const students = [
@@ -14,24 +14,42 @@ function App() {
     { name: "Jenny", age: 25, grade: "D" },
     { name: "Kevin", age: 26, grade: "E" },
     { name: "Linda", age: 27, grade: "F" },
-  ];
+  ]
 
-  const [minAge, setMinAge] = useState(18);
+  const [minAge, setMinAge] = useState(18)
 
   // TODO: filter를 사용하여 minAge 이상의 학생들만 선택하세요.
-  // const filteredStudents = ...;
+  const filteredStudents = students.filter((student) => student.age >= minAge)
+
+  const minAgeHandler = (e) => {
+    if (e.target.value < 0) {
+      alert("나이는 0 이하로 입력할 수 없습니다.")
+      e.target.value = ""
+      return
+    }
+    setMinAge(e.target.value || minAge)
+  }
 
   return (
     <div>
       <h1>학생 목록</h1>
       다음 나이 이상의 학생목록만 출력해요 :{" "}
       {/* TODO: input에 입력된 값(숫자) 이상의 나이를 가진 학생들만 출력하세요. */}
-      <input type="number" /> 살 이상
+      <input type="number" onChange={minAgeHandler} /> 살 이상
       <ul>
         {/* TODO: map을 사용하여 필터링된 학생들의 정보를 표시하세요. 동명이인은 없다고 가정합니다. ex) 홍길동 - Age: 24, Grade: A */}
+        {filteredStudents.map((student) => {
+          return (
+            <li key={student.name}>
+              <p>
+                {student.name} - Age: {student.age}, Grade: {student.grade}
+              </p>
+            </li>
+          )
+        })}
       </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
